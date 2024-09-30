3d57fafa4f209879e4ef97a7826d2ca371f1364b
MiikkaVeeee <miikka.t.venalainen@tuni.fi>
2024-09-30 16:29:49 +0300

    Revert "Merge branch 'main' of https://github.com/timimn/Multisensory-VR-Learning-Game"
    This reverts commit 4dc105653720d343ed415af35258fc2e23ee33cb, reversing
    changes made to d50f4e1f4e1a029fb2fd70eb2dbfed22a220a1aa.

--- Assets/Scripts/Interactable Objects/FireExtinguisherBehaviour.cs
+++ Assets/Scripts/Interactable Objects/FireExtinguisherBehaviour.cs
@@ -3,32 +3,22 @@ using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
-using TMPro;
 
 public class FireExtinguisherBehaviour : MonoBehaviour {
-    public Transform gameCamera;
     public ParticleSystem foamParticleSystem;
     public XRGrabInteractable grabInteractable;
     public XRBaseInteractor leftControllerInteractor;
     public XRBaseInteractor rightControllerInteractor;
-    public GameObject infoCanvas;
-    public TextMeshProUGUI infoText;
 
     private InputDevice leftController;
     private InputDevice rightController;
     private bool rightControllerGrabbing = true;
-    private int hoveringControllerCount = 0;
-    private float canvasHeightOffset = 0.6f;
-    private const string objectName = "Fire Extinguisher";
-    private const string objectDescription = "Grab: [<sprite=8>/<sprite=10>]";
 
     // Start is called before the first frame update
     void Start() {
         leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
         rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
         grabInteractable.selectEntered.AddListener(OnSelectEntered);
-        grabInteractable.hoverEntered.AddListener(OnHoverEntered);
-        grabInteractable.hoverExited.AddListener(OnHoverExited);
     }
 
     // Update is called once per frame
@@ -42,11 +32,7 @@ public class FireExtinguisherBehaviour : MonoBehaviour {
             rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
         }
         
-        // If the object is being hovered over, update the canvas to face the camera
-        if (hoveringControllerCount > 0) {
-            infoCanvas.transform.position = this.transform.position + this.transform.up * canvasHeightOffset;
-            infoCanvas.transform.rotation = Quaternion.LookRotation(gameCamera.forward);
-        } else if (grabInteractable.isSelected) {
+        if (grabInteractable.isSelected) {
             float triggerValue = 0f;
             bool triggerPressed = false;
 
@@ -68,26 +54,6 @@ public class FireExtinguisherBehaviour : MonoBehaviour {
         }
     }
 
-    // Event listener, for when the object is hovered over
-    private void OnHoverEntered(HoverEnterEventArgs args) {
-        hoveringControllerCount++;
-
-        // If the object is being hovered over without being grabbed, display object information
-        if (hoveringControllerCount > 0 && !grabInteractable.isSelected) {
-            infoText.text = $"{objectName}\n{objectDescription}";
-            infoCanvas.SetActive(true);
-        }
-    }
-
-    // Event listener, for when the object is no longer hovered over
-    private void OnHoverExited(HoverExitEventArgs args) {
-        hoveringControllerCount--;
-
-        if (hoveringControllerCount < 1) {
-            infoCanvas.SetActive(false);
-        }
-    }
-
     // Event listener, for when the object is selected
     private void OnSelectEntered(SelectEnterEventArgs args) {
         // Get the interactor that selected the object
@@ -99,13 +65,10 @@ public class FireExtinguisherBehaviour : MonoBehaviour {
         } else if (interactor == leftControllerInteractor) {
             rightControllerGrabbing = false;
         }
-        infoCanvas.SetActive(false);
     }
 
     // Function for cleaning up listeners, should the object be destroyed
     private void OnDestroy() {
         grabInteractable.selectEntered.RemoveListener(OnSelectEntered);
-        grabInteractable.hoverEntered.RemoveListener(OnHoverEntered);
-        grabInteractable.hoverExited.RemoveListener(OnHoverExited);
     }
 }
